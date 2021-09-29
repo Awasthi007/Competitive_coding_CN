cf81560e0496fec5ccc708dc561dc643e6e5d0b9
Shashank Awasthi <62172664+Awasthi007@users.noreply.github.com>
2021-09-29 09:46:37 +0530

    Update quick_sort.cpp

--- advanced_recursion/quick_sort.cpp
+++ advanced_recursion/quick_sort.cpp
@@ -1,1 +1,30 @@
+Quick Sort Code
+Send Feedback
+Sort an array A using Quick Sort.
+Change in the input array itself. So no need to return or print anything.
 
+
+Input format :
+First line will contain T number of test case and each tet case will consist of two lines.
+Line 1 : Integer n i.e. Array size
+Line 2 : Array elements (separated by space)
+Output format :
+for every test case print rray elements in increasing order (separated by space) in a new line.
+Constraints :
+1 <= T <= 10
+1 <= n <= 10^5
+0 <= arr[i] <= 10^9
+Sample Input 1 :
+1
+6 
+2 6 8 5 4 3
+Sample Output 1 :
+2 3 4 5 6 8
+Sample Input 2 :
+1
+5
+1 5 2 7 3
+Sample Output 2 :
+1 2 3 5 7 
+  
+  ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
